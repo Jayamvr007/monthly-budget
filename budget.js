96b78bb55e765be327e7dae3b864487bcce85f70
Jayamvr007 <49724054+Jayamvr007@users.noreply.github.com>
2019-09-23 22:00:13 +0530

    Add files via upload

---
+++ budget.js
@@ -0,0 +1,13 @@
+var total,income,expenses,in[20],ex[20];
+
+	if(document.querySelector('#add').value =='+')
+	{
+		document.querySelector('#val').addEventListener("keydown", function (e) {
+    if (e.keyCode === 13) 
+    		{  //checks whether the pressed key is "Enter"
+        		in.push()
+				document.querySelector('.in').textContent = document.querySelector("#des").value;
+			}
+		});
+	}
+
