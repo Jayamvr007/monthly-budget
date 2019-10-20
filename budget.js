f4143b3a2acca637794368b90efc26ff53875aa2
Jayamvr007 <49724054+Jayamvr007@users.noreply.github.com>
2019-10-20 16:57:54 +0530

    Add files via upload

--- budget.js
+++ budget.js
@@ -1,7 +1,14 @@
- var income=0,expenses=0,total=0,v1,v2;
+ var income=0,expenses=0,total=0,v1,v2,c=1;
+ document.getElementById("total").innerHTML=total;
 
+ 
 function budget()
 		{	
+			 var x = document.getElementById("val").value;
+  if (x =='') {
+    alert("value must be filled out");
+    return false;
+  }
 			
 			if(document.getElementById("sel").value=='+')
 			{
@@ -10,8 +17,18 @@ function budget()
 				var newp=document.createElement('p');
 				newp.textContent=document.querySelector('#des').value;
 				document.getElementById("des1").appendChild(newp);
+				var newp=document.createElement('p');
+				newp.textContent=document.querySelector('#val').value;
+				document.getElementById("val1").appendChild(newp);
+				income+=parseInt(document.getElementById("val").value);
+				document.getElementById("ti").innerHTML="+"+income;
+				var btn=document.createElement('button');
+				btn.innerHTML='-';
+				document.getElementById("cross1").appendChild(btn);
+				var newline=document.createElement('p');
+				document.getElementById("cross1").appendChild(newline);
 				
-
+				
 			}
 			else
 			{
@@ -21,37 +38,26 @@ function budget()
 				var newp=document.createElement('p');
 				newp.textContent=document.querySelector('#des').value;
 				document.getElementById("des2").appendChild(newp);
-				
-			}
-			if(document.getElementById("sel").value=='+')
-			{
-			
-			
-				var newp=document.createElement('p');
-				newp.textContent=document.querySelector('#val').value;
-				document.getElementById("val1").appendChild(newp);
-				income+=parseInt(document.getElementById("val").value);
-				document.getElementById("ti").innerHTML=income;
-				
-
-				
-
-			}
-			else
-			{
-
-			
-			
 				var newp=document.createElement('p');
 				newp.textContent=document.querySelector('#val').value;
 				document.getElementById("val2").appendChild(newp);
 				expenses+=parseInt(document.getElementById("val").value);
-				document.getElementById("te").innerHTML=expenses;
+				document.getElementById("te").innerHTML="-"+expenses;
+				var btn=document.createElement('button');
+				btn.innerHTML='-';
+				document.getElementById("cross2").appendChild(btn);
+				var newline=document.createElement('p');
+				document.getElementById("cross2").appendChild(newline);
+				
+
 				
 			}
+			
 			total=income-expenses;
+			if(total>0)
+			document.getElementById("total").innerHTML="+"+total;
+		else
 			document.getElementById("total").innerHTML=total;
-
 			
 			
 
